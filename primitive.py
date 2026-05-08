b111af5fc798ef1420e6693699dec56fdd85947a
MARK <joxa030@gmail.com>
2026-05-08 23:12:28 +0900

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -6,6 +6,33 @@ count = 100
 count_type = type(count)
 print(f"the count: {count} and:{count_type}")
 
-result1 = count.bit_count() #method   # .nuqta bosilganda uning state va methodlari chiadi
-result2 = count.numerator #state 
+# method   # .nuqta bosilganda uning state va methodlari chiadi
+result1 = count.bit_count()
+result2 = count.numerator  # state
 print(result1, result2)
+
+print("===string===")
+# METHODS: upper() .lower() .title() .find() .replace()
+
+
+course = "AI Python fulStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+#course.  # shu xolatda foyda li methodlarni ro'yxatini ko'rish mumkun
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.lower()
+print(f"the result (4): {result}")
+
+result = course.replace("fulStack", "MasterClass")
+print(f"the result (5): {result}")
+
+
+
+print("============")
