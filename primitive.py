a90bd52b0798740b6f31ddf99b8a5f11805a65f5
MARK <joxa030@gmail.com>
2026-05-08 22:50:28 +0900

    feat: lern numbers in python

---
+++ primitive.py
@@ -0,0 +1,11 @@
+print("==========")
+# in JAVA variable is a name storage location, m m nomlanishi
+# in PYTHON, variable is named reference!, object
+
+count = 100
+count_type = type(count)
+print(f"the count: {count} and:{count_type}")
+
+result1 = count.bit_count() #method   # .nuqta bosilganda uning state va methodlari chiadi
+result2 = count.numerator #state 
+print(result1, result2)
