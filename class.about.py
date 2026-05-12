376ba1f5af72901340d75b6cad48c0f9fb895aaf
MARK <joxa030@gmail.com>
2026-05-12 18:30:36 +0900

    feat: learn class and ist ordinary and statig property

---
+++ class.about.py
@@ -0,0 +1,54 @@
+''' CLASS
+    (1) What is class
+    (2) ordinary vs static properties
+    (3) special methods
+'''
+
+print("===== What is class =====")
+# classlar object yasovchi shablon
+# classlar 3xil turdagi strukturaga ega > state constructor method
+
+
+class Person():
+    # state
+     message = "static state property"
+
+# constructor
+
+     def __init__(self, name, age):  # self bu this
+        self.name = name
+        self.age = age
+
+    # method
+     def introduce(self):
+        print(f"{self.name} says: How do you do!")
+
+     def say_age(self):
+        print(f"{self.name} says I am {self.age}!")
+        
+        
+     @classmethod
+     def explain(cls):
+        print("static method property executed!")
+
+
+person1 = Person("Justin", 25)
+person2 = Person("Martin", 35)
+person3 = Person("Jhon", 22)
+
+# odatiy state
+print("person1.name", person1.name)
+
+# odatiy method
+person1.introduce()
+person2.say_age()
+
+
+print("===== ordinary vs static properties =====")
+# static state
+
+new_message = Person.message
+print("new_message", new_message)
+
+#static method 
+Person.explain()
