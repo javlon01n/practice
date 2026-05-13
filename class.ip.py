0fceb917dddf69c73c7c012c29dece72c0d3fdb7
MARK <joxa030@gmail.com>
2026-05-14 06:02:42 +0900

    feat: POLIMORPHISM concept

--- class.ip.py
+++ class.ip.py
@@ -16,15 +16,15 @@ class Animal:  # PARENT
     def __init__(self, voise):
         self._status = "animal is alive"
         self.voise = voise
-        
-        
-    #method 
+
+    # method
+
     def make_voise(self):
         print(f"the animal can make voice: {self.voise}")
 
 
-class Dog(Animal): #child
-    #state         
+class Dog(Animal):  # child
+    # state
 
     # constructor
 
@@ -32,19 +32,21 @@ class Dog(Animal): #child
         self.name = name
         self.sound = sound
         super().__init__(voise)
-        
-    #method 
+
+    # method
     def introduce(self):
         print(f"{self.name} says: {self.sound}-{self.sound}")
-
 
     def protect(self):
         print("Yes, i can protect you!")
-        
-        
-        
-class Cat(Animal): #child
-    #state         
+
+    def make_voise(self):
+        print(f"the {self.name} says {self.sound}")
+
+
+
+class Cat(Animal):  # child
+    # state
 
     # constructor
 
@@ -52,19 +54,17 @@ class Cat(Animal): #child
         self.name = name
         self.sound = sound
         super().__init__(voise)
-        
-    #method 
+
+    # method
     def introduce(self):
         print(f"{self.name} says: {self.sound}-{self.sound}")
-
 
     def protect(self):
         pass
-        
-        
-        
-class Fish(Animal): #child
-    #state         
+
+
+class Fish(Animal):  # child
+    # state
 
     # constructor
 
@@ -72,24 +72,22 @@ class Fish(Animal): #child
         self.name = name
         self.sound = sound
         super().__init__(voise)
-        
-    #method 
+
+    # method
     def introduce(self):
         print(f"{self.name} says: {self.sound}-{self.sound}")
 
-
     def protect(self):
         print("Yes i can swim!")
-        
-        
-        
-dog = Dog("Rex", "wow", True)        
-cat = Cat("Tom", "myeow", True)        
-fish = Fish("Nemo", "zZz", False)     
+
+
+dog = Dog("Rex", "wow", True)
+cat = Cat("Tom", "myeow", True)
+fish = Fish("Nemo", "zZz", False)
 
 dog.introduce()
 cat.introduce()
-fish.introduce()   
+fish.introduce()
 
 print("-----------")
 dog.make_voise()
@@ -101,4 +99,15 @@ print(Dog.description)
 print(dog.voise, fish.voise)
 print("dog.status:", dog._status)
 print("cat.status:", cat._status)
-        
+
+
+print("===== POLIMORPHISM =====")
+
+dog.make_voise()
+fish.make_voise()
+
+print("______")
+# fish >Fish Animal > object()
+data1 = issubclass(Fish, Animal)
+data2 = issubclass(Animal, object)
+print("data:", data1, data2)
