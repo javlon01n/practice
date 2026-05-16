c2216ee7a8bcc00e5046da58f588e126204c3dbc
MARK <joxa030@gmail.com>
2026-05-16 18:42:14 +0900

    feat: learn list & its method

---
+++ lists.py
@@ -0,0 +1,89 @@
+'''List 
+    (1) Working with lists
+    (2) list methods
+    (3) Lambda function
+    (4) enumarate, map and filter
+'''
+
+print("====== Working with lists ======")
+# Java/PHP/NodeJS array deyiladi => Pythonda esa list
+
+# literal
+person = {"name": "Justin", "age": 25}  # dictionary
+people = ("Andrew", "FLEX", "DEVEX", "MG")  # tuple
+groups = ["MIT", "FLEX", "DEVEX", "MG"]  # list
+for team in groups:
+    print(f"the team: {team}")
+
+
+# constructor
+letters = list("Hello World!")
+print(f"the letters: {letters} and size: {len(letters)}")
+
+print("------")
+fruits = ["apple", "orange", "lemon", "kiwi"]
+
+a = fruits[0]
+b = fruits[0:2]
+c = fruits[::3]
+d = fruits[::-1]
+
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("d:", d)
+
+
+print("====== list methods ======")
+# method > append() .insert() .pop() .remove() .clear() .sort() .index(value)
+
+letters = ["a", "d", "b"]
+
+letters.append("c")  # add behind
+print(f"the append letters: {letters}")
+
+letters.insert(0, "z")  # add front
+print(f"the insert letters: {letters}")
+
+size = len(letters) - 1
+resualt1 = letters.pop(size)  # pop behind
+print(f"the pop resualt1: {resualt1} and letters: {letters}")
+
+resualt2 = letters.pop(0)  # pop front
+print(f"the pop resualt2: {resualt2} and letters: {letters}")
+
+
+print("---------")
+animals = ["dog", "cat", "capybara" "fish", "lion"]
+print("animals:", animals)
+
+animals.remove("lion")
+print("animals remove:", animals)
+
+del animals[2:4]
+print("animals delete:", animals)
+
+exist = animals.index("cat")
+print("cat exist:", exist)
+
+animals.clear()
+print("animals clear:", animals)
+
+if "cat" in animals:
+    print("index of cat:", animals.index("cat"))
+else:
+    print("cat does not exist")
+
+
+
+print("---------")
+numbers = [2, 20, 12, 8, 57]
+numbers.sort()
+print("sort default:", numbers)
+numbers.sort(reverse=True)
+print("sort reverse:", numbers)
+
+# immuteble sorted  immuteble > sorti function & .index(value) method 
+numbs = [2, 20, 12, 100]
+new_numbs = sorted(numbs)
+print(f"the sorted numbs: {numbs} and new_numbs: {new_numbs}")
