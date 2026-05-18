4ddb043a9a7567f17fbab896cafdbb792a0cc87a
MARK <joxa030@gmail.com>
2026-05-18 23:50:06 +0900

    fix: Specific operators with set

--- array.set.py
+++ array.set.py
@@ -4,8 +4,8 @@
     (3) Specific operators with set 
 '''
 
-print("====== Array ======")
 from array import array
+print("====== Array ======")
 numbers = array("i", [1, 4, 5, 7, 8, 41])
 print("numbers(1):", numbers)
 
@@ -19,3 +19,34 @@ print("numbers(3)", numbers)
 
 del numbers[0:2]
 print("numbers(4):", numbers)
+
+
+print("====== Set ======")
+# set of unique collection without keeping order      set takroriy sonlarni faqat bir marta qabul qiladi
+new_numbers = array("i", [1, 4, 7, 5, 7, 5, 4, 7, 8, 4, 41])
+numbs_set = set(new_numbers)
+
+print(f"the numbs_set: {numbs_set} and type: {type(numbs_set)}")
+
+numbs_set.add(200)
+print("numbs_set(1):", numbs_set)
+
+numbs_set.add(7)
+print("numbs_set(2):", numbs_set)
+
+
+print("====== Specific operators  ======")
+# | & - ^
+
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b  # union yaniy | bu belgi a va b qiymatidagi sonlarni xammasini keltiradi faqat takroriy sonlarni bir marta yozadi
+result2 = a & b  # intersection yaniy & bu belgi a va b qiymatlaridagi takrorlaniuvchi sonni olib beradi >20
+result3 = a - b  # difference - bu belgi a va b dagi birxil belgilarni olib tashlab faqat birinchi kelayotgan xarf a ni oqiydi natija/ 10 , 50
+result4 = a ^ b # symmetric difference ^ bu belgi a va b dagi takrorlanmagan sonlarni o'qiydi natija/ 1 40, 10, 50
+
+print("result1:", result1)
+print("result2:", result2)
+print("result3:", result3)
+print("result4:", result4)
